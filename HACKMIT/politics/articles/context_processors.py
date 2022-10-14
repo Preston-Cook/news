14107f800d4db01632759171e9e07d18807c8775
Preston Cook <preston.l.cook@gmail.com>
2022-10-14 17:53:45 -0500

    Delete HACKMIT/politics directory

--- HACKMIT/politics/articles/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .utils import categories
-
-def custom_ctx(request):
-    return {
-        'categories': [category.title() for category in categories]
-    }
--- HACKMIT/politics/articles/static/articles/js/register.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('#register-link').setAttribute('class', 'nav-link active')
-})
-
